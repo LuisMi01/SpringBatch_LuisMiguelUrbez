40a210144633903a79b28d1f8caf9d173aca3310
The Chief <lurbevil@myuax.com>
2023-12-10 18:32:08 +0100

    error en el dcoker

--- backend/src/main/java/io/uax/backend/repos/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package io.uax.backend.repos;
-
-import io.uax.backend.domain.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UsuarioRepository extends MongoRepository<Usuario, Long> {
-}
