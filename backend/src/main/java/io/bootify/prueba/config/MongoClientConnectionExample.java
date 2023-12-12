44765020326348d79d935b1d26ee8d21c5ee40ad
The Chief <lurbevil@myuax.com>
2023-12-12 13:31:02 +0100

    Tratando de conectar la base de datos

---
+++ backend/src/main/java/io/bootify/prueba/config/MongoClientConnectionExample.java
@@ -0,0 +1,38 @@
+package io.bootify.prueba.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoClientConnectionExample {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://lmurbezv:LuisMiguel2303@clusterbanco.1vyevyh.mongodb.net/?retryWrites=true&w=majority";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Conectado a la base de datos!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
