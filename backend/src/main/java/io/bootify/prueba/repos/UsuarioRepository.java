bd9139d21ae37f135115fc19d01390033e6bd6eb
The Chief <lurbevil@myuax.com>
2023-12-12 17:58:29 +0100

    Por fallos en la bae de datos he vuelto a importar el proyecto desde spring Inizialazer y vuelvo a comenzar con la logica concurrente de java

--- backend/src/main/java/io/bootify/prueba/repos/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package io.bootify.prueba.repos;
-
-import io.bootify.prueba.domain.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UsuarioRepository extends MongoRepository<Usuario, Long> {
-}
