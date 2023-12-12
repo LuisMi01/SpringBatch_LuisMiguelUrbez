d9b7df9bb98fa5e679178451ca501ab0275ac789
The Chief <lurbevil@myuax.com>
2023-12-12 20:14:40 +0100

    LOS USUARIOS SE MUESTRAN POR POSTAM

--- backend/src/main/java/com/uax/backend/controller/UsuarioController.java
+++ backend/src/main/java/com/uax/backend/controller/UsuarioController.java
@@ -1,4 +1,40 @@
 package com.uax.backend.controller;
 
+import com.uax.backend.model.Usuario;
+import com.uax.backend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios() {
+        return usuarioService.getAllUsuarios();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getUsuarioById(@PathVariable String id) {
+        return usuarioService.getUsuarioById(id);
+    }
+
+    @PostMapping
+    public Usuario saveUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.saveUsuario(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable String id) {
+        usuarioService.deleteUsuario(id);
+    }
 }
