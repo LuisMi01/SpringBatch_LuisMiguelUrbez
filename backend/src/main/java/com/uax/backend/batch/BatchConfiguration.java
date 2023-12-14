6606b1975e9748962807c275101ee421c872331b
The Chief <lurbevil@myuax.com>
2023-12-14 19:55:30 +0100

    El step  no esta funcionando correctamente

--- backend/src/main/java/com/uax/backend/batch/BatchConfiguration.java
+++ backend/src/main/java/com/uax/backend/batch/BatchConfiguration.java
@@ -4,12 +4,12 @@ import com.uax.backend.model.Transaction;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.data.MongoItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -19,15 +19,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
-
-    @Autowired
-    private JobBuilder jobBuilder;
-    @Autowired
-    private StepBuilder stepBuilder;
 
     @Autowired
     private FlatFileItemReader<Transaction> reader;
@@ -70,26 +66,23 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-        return jobBuilderFactory.get("importUserJob")
-                .incrementer(new RunIdIncrementer())
+    public Job importUserJob(JobRepository jobRepository, Step step1, JobCompletionNotificationListener listener) {
+        return new JobBuilder("importUserJob", jobRepository)
                 .listener(listener)
                 .start(step1)
                 .build();
     }
+
     @Bean
-    public Step step1(MongoItemWriter<Transaction> writer, FlatFileItemReader<Transaction> reader, TransactionProcessor processor) {
-        return stepBuilderFactory.get("step1")
-                .<Transaction, Transaction> chunk(10)
+    public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+                      FlatFileItemReader<Transaction> reader, TransactionProcessor processor, JdbcBatchItemWriter<Transaction> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<Transaction, Transaction> chunk(3, transactionManager)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
                 .build();
     }
-
-
-
-
 
 
 }
