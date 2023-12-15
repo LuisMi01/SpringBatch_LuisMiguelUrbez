5f23e4109ef787138d93d01a30398bbb9fb0392a
The Chief <lurbevil@myuax.com>
2023-12-15 12:57:36 +0100

    Cambio de modelo de bases de datos a un modelo relacional. Dejando atras a MongoDB y entrando a utilizar postgresSQL

--- backend/src/main/java/com/uax/backend/repos/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uax.backend.repos;
-
-import com.uax.backend.model.Transaction;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends MongoRepository<Transaction, String> {
-}
