26c5ed2e52a23434d3ac827446ac1eaf1eafc016
The Chief <lurbevil@myuax.com>
2023-12-22 12:25:57 +0100

    Cambios en el filtro v2

--- target/classes/templates/filtros/filtrosjs.js
+++ target/classes/templates/filtros/filtrosjs.js
@@ -1,8 +1,16 @@
 function applyFilter(filterType) {
-    fetch('/filter?filterType=' + filterType)
+    fetch('/filtros?filterType=' + filterType)
         .then(response => response.json())
         .then(users => {
             // Aquí debes implementar la lógica para actualizar la lista de usuarios en la página
             // con la lista filtrada de usuarios
+            var userList = document.getElementById('user-list');
+            userList.innerHTML = ''; // Limpiar la lista de usuarios existente
+            users.forEach(user => {
+                // Crear un nuevo elemento para cada usuario y añadirlo a la lista
+                var userElement = document.createElement('div');
+                userElement.textContent = user.accountId + ' ' + user.transactionType + ' ' + user.amount + ' ' + user.transactionDate;
+                userList.appendChild(userElement);
+            });
         });
 }
